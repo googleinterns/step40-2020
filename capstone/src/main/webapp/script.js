d352d46e00246f2ed020f6b8d78ea8b3893a4bc8
chrismohrii <65875732+chrismohrii@users.noreply.github.com>
2020-07-09 15:22:37 -0400

    Interactive Attribute Selection (#13)
    * Display basic text output for Perspective
    
    * Test
    
    * Post request to Perspective
    
    * Remove greeting in index.html
    
    * Remove hardcoding in servlet
    
    * Remove API key
    
    * Add user input box for servlet
    
    * Remove API key
    
    * Add newline at end of file
    
    * Remove API key
    
    * Add on Charts
    
    * Remove unnecessary space
    
    * Add interactive attribute selection
    
    * Remove key
    
    * Remove text output
    
    * Fix intern Daniel's nits
    
    * Fix Olu's nit
    
    * Implement Olu's style suggestion
    
    * Fix Lucas's comments
    
    * Commit Lucas's suggestion: let --> const
    
    Co-authored-by: lgsds <55889299+lgsds@users.noreply.github.com>
    
    Co-authored-by: Daniel O <danieldortega77>
    Co-authored-by: lgsds <55889299+lgsds@users.noreply.github.com>

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -12,44 +12,107 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Call the perspective API
- */
-async function callPerspective(text, lang) {
-  const response = await fetch('/call_perspective', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json',},
-      body: JSON.stringify({text: text, lang: lang})});
-  const toxicityData = await response.json();
-  displayPerspectiveOutput(toxicityData);
-}
+const ATTRIBUTES_BY_LANGUAGE = {
+  'en': ['TOXICITY', 'SEVERE_TOXICITY', 'TOXICITY_FAST', 'IDENTITY_ATTACK', 'INSULT', 'PROFANITY', 'THREAT', 'SEXUALLY_EXPLICIT', 'FLIRTATION'],
+  'es': ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK_EXPERIMENTAL', 'INSULT_EXPERIMENTAL', 'PROFANITY_EXPERIMENTAL', 'THREAT_EXPERIMENTAL'],
+  'fr': ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK_EXPERIMENTAL', 'INSULT_EXPERIMENTAL', 'PROFANITY_EXPERIMENTAL', 'THREAT_EXPERIMENTAL'],
+  'de': ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK', 'INSULT', 'PROFANITY', 'THREAT'],
+  'it': ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK', 'INSULT', 'PROFANITY', 'THREAT'],
+  'pt': ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK', 'INSULT', 'PROFANITY', 'THREAT']
+};
 
-/**
- *  Display toxicity output on the webpage
- */
-function displayPerspectiveOutput(toxicityData) {
-  const outputElement = document.getElementById('perspective-output-container');
-  if (!outputElement) {
+/** Collects the user's input and calls Perspective on it */
+async function submitInput() {
+  const textElement = document.getElementById('textForAnalysis');
+  if (!textElement) {
     return;
   }
-  outputElement.innerHTML = '';
-
-  if (toxicityData.attributeScores) {
-    for (let key in toxicityData.attributeScores) {
-      if (toxicityData.attributeScores[key].summaryScore && 
-          toxicityData.attributeScores[key].summaryScore.value) {
-        attributeElement = createAnyElement('p', key + ": " + toxicityData.attributeScores[key].summaryScore.value);
-        outputElement.appendChild(attributeElement);
-      }
-    }
+  const langElement = document.getElementById('languageForAnalysis');
+  if (!langElement) {
+    return;
   }
+  const attributes = document.getElementById("available-attributes").getElementsByTagName('input');
+  const requestedAttributes = []
+  for (const attribute of attributes) {
+    if (attribute.checked) {
+      requestedAttributes.push(attribute.value);	
+    }	
+  }
+  await callPerspective(textElement.value, langElement.value, requestedAttributes);
 }
 
-/**
- * Create a 'tag' element with 'text' as its inner HTML
- */
-function createAnyElement(tag, text) {
-  const textElement = document.createElement(tag);
-  textElement.innerHTML = text;
-  return textElement;
+/** Calls the perspective API */
+async function callPerspective(text, lang, requestedAttributes) {
+  const response = await fetch('/call_perspective', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json',},
+    body: JSON.stringify({text: text, lang: lang, requestedAttributes: requestedAttributes})});
+  const toxicityData = await response.json();
+  loadChartsApi(toxicityData);
+}
+
+/** Loads the Google Charts API */
+function loadChartsApi(toxicityData) {
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(function() {drawBarChart(toxicityData);}); 
+}
+
+/** Draws a Google BarChart from a Perspective JSON. */
+function drawBarChart(toxicityData) {
+  document.getElementById('chart-container').innerHTML = '';
+  const data = google.visualization.arrayToDataTable([[{label: 'Attribute'}, {label: 'Score', type: 'number'}, {role: "style"}]]);
+
+  Object.keys(toxicityData.attributeScores).forEach((attribute) => {
+    var color = '#6B8E23'; // Green
+    const score = toxicityData.attributeScores[attribute].summaryScore.value;
+    if (score >= 0.8) {
+      color = '#DC143C'; // Red
+    } else if (score >= 0.2) {
+      color = '#ffd800'; // Yellow
+    }
+    data.addRow([attribute, score, color]);
+  });
+
+  data.sort({column: 1, desc: false});
+
+  const options = {
+    title: 'Perspective Feedback',
+    bars: 'horizontal',
+    height: 700,
+    legend: {position: "none"},
+    theme: 'material', 
+    hAxis: {viewWindow: {min: 0, max: 1}}
+  };
+
+  const chart = new google.visualization.BarChart(document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
+
+/** Shows the avaiable attributes given a language selected on text analyzer page */
+function showAvailableAttributes() {
+  const langElement = document.getElementById('languageForAnalysis');
+  if (!langElement) {
+    return;
+  }
+  const lang = langElement.value;
+  const avaiableAttributesElement = document.getElementById('available-attributes');
+  avaiableAttributesElement.innerHTML = '';
+	
+  const attributes = ATTRIBUTES_BY_LANGUAGE[lang];
+  attributes.forEach(function(attribute) {
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.value = attribute;
+    checkbox.id = attribute + '-checkbox';
+    checkbox.checked = true;
+  
+    const label = document.createElement('label');
+    label.htmlFor = attribute + '-checkbox';
+    label.appendChild(document.createTextNode(attribute));
+  
+    avaiableAttributesElement.appendChild(checkbox);
+    avaiableAttributesElement.appendChild(document.createTextNode(" "));
+    avaiableAttributesElement.appendChild(label);
+    avaiableAttributesElement.appendChild(document.createTextNode(" "));
+  });
 }
