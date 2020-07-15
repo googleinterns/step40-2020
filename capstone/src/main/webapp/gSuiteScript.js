d8daf2cbf49af03d7513df3fa9a9bafa6c86bff2
Daniel O <danieldortega77>
2020-07-15 20:26:32 +0000

    Create new sheet

--- capstone/src/main/webapp/gSuiteScript.js
+++ capstone/src/main/webapp/gSuiteScript.js
@@ -12,6 +12,106 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Client ID and API key from the Developer Console
+const CLIENT_ID = '829584540184-uhjij62s65igq2r5n29sevp1ehbu0u93.apps.googleusercontent.com';
+const DOCS_API_KEY = 'AIzaSyD2womLpvJAC11BQJiLRMCBbP4pwy4bJu8'; // TODO: Create Java servlet to return key
+const SHEETS_API_KEY = 'AIzaSyDMRYIXlcVWOVh-TTvrpVl11KTIw14Mg3c'; // TODO: Create Java servlet to return key
+
+// Array of API discovery doc URLs for APIs
+const DOCS_DISCOVERY_DOCS = ["https://docs.googleapis.com/$discovery/rest?version=v1"];
+const SHEETS_DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = "https://www.googleapis.com/auth/documents.readonly https://www.googleapis.com/auth/spreadsheets";
+
+let authorizeButton = document.getElementById('authorize_button');
+let signoutButton = document.getElementById('signout_button');
+let submitButton = document.getElementById('submit_button');
+let currentChoice = 'docs';
+
+/**
+  *  On load, called to load the auth2 library and API client library.
+  */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+  *  Initializes the API client library and sets up sign-in state
+  *  listeners.
+  */
+function initClient() {
+  let apiKey = '';
+  discoveryDocs = [];
+  if (currentChoice === 'docs') {
+    apiKey = DOCS_API_KEY;
+    discoveryDocs = DOCS_DISCOVERY_DOCS;
+  } else if (currentChoice === 'sheets') {
+    apiKey = SHEETS_API_KEY;
+    discoveryDocs = SHEETS_DISCOVERY_DOCS;
+  }
+
+  gapi.client.init({
+    apiKey: apiKey,
+    clientId: CLIENT_ID,
+    discoveryDocs: discoveryDocs,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  }, function(error) {
+    console.log(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+  *  Called when the signed in status changes
+  *  to update the UI appropriately.
+  */
+async function updateSigninStatus(isSignedIn) {
+  authorizeButton = document.getElementById('authorize_button');
+  signoutButton = document.getElementById('signout_button');
+  submitButton = document.getElementById('submit_button');
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    submitButton.style.display = 'block';
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+    submitButton.style.display = 'none';
+  }
+}
+
+/**
+  *  Sign in the user upon button click.
+  */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+  *  Sign out the user upon button click.
+  */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+function updateChoice() {
+  const choiceElement = document.getElementById('medium-of-input');
+  if (choiceElement == null) {
+    return;
+  }
+  currentChoice = choiceElement.value;
+  handleClientLoad();
+}
+
 /** Collects the user's input and submits it for analysis */
 async function gatherGSuiteInput() {
   // Get the submitted id and language
@@ -51,9 +151,9 @@ async function gatherGSuiteInput() {
   }
 
   let text = '';
-  if (document.getElementById('medium-of-input').value === 'docs') {
+  if (currentChoice === 'docs') {
     text = await getTextFromDoc(id);
-  } else if (document.getElementById('medium-of-input').value === 'sheets') {
+  } else if (currentChoice === 'sheets') {
     text = await getTextFromSheet(id);
   }
 
@@ -131,92 +231,30 @@ function readParagraphElement(paragraphElement) {
   return run.content;
 }
 
-// Client ID and API key from the Developer Console
-const CLIENT_ID = 'CLIENT_ID';
-const DOCS_API_KEY = 'API_KEY'; // TODO: Create Java servlet to return key
-const SHEETS_API_KEY = 'API_KEY'; // TODO: Create Java servlet to return key
-
-// Array of API discovery doc URLs for APIs
-const DOCS_DISCOVERY_DOCS = ["https://docs.googleapis.com/$discovery/rest?version=v1"];
-const SHEETS_DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-const SCOPES = "https://www.googleapis.com/auth/documents.readonly https://www.googleapis.com/auth/spreadsheets.readonly";
-
-let authorizeButton = document.getElementById('authorize_button');
-let signoutButton = document.getElementById('signout_button');
-let submitButton = document.getElementById('submit_button');
-
-/**
-  *  On load, called to load the auth2 library and API client library.
-  */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-/**
-  *  Initializes the API client library and sets up sign-in state
-  *  listeners.
-  */
-function initClient() {
-  let apiKey = '';
-  discoveryDocs = [];
-  if (document.getElementById('medium-of-input').value === 'docs') {
-    apiKey = DOCS_API_KEY;
-    discoveryDocs = DOCS_DISCOVERY_DOCS;
-  } else if (document.getElementById('medium-of-input').value === 'sheets') {
-    apiKey = SHEETS_API_KEY;
-    discoveryDocs = SHEETS_DISCOVERY_DOCS;
-  }
-
-  gapi.client.init({
-    apiKey: apiKey,
-    clientId: CLIENT_ID,
-    discoveryDocs: discoveryDocs,
-    scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  }, function(error) {
-    console.log(JSON.stringify(error, null, 2));
+async function createSheet(title, data) {
+  const oldChoice = currentChoice;
+  currentChoice = 'sheets';
+  handleClientLoad();
+  const response = await gapi.client.sheets.spreadsheets.create({
+    properties: {
+      title: title
+    }
   });
+  const id = await response.result.spreadsheetId;
+  appendDataToSheet(id, data);
+  currentChoice = oldChoice;
+  handleClientLoad();
 }
 
-/**
-  *  Called when the signed in status changes
-  *  to update the UI appropriately.
-  */
-async function updateSigninStatus(isSignedIn) {
-  authorizeButton = document.getElementById('authorize_button');
-  signoutButton = document.getElementById('signout_button');
-  submitButton = document.getElementById('submit_button');
-  if (isSignedIn) {
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
-    submitButton.style.display = 'block';
-  } else {
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
-    submitButton.style.display = 'none';
-  }
-}
-
-/**
-  *  Sign in the user upon button click.
-  */
-function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
-  *  Sign out the user upon button click.
-  */
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
+async function appendDataToSheet(spreadsheetId, data) {
+  const body = {
+    values: data
+  };
+  const response = await gapi.client.sheets.spreadsheets.values.append({
+    spreadsheetId: spreadsheetId,
+    range: 'Sheet1!A1:YY',
+    valueInputOption: 'USER_ENTERED',
+    resource: body
+  });
+  const result = await response.result;
 }
