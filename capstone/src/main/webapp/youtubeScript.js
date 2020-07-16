98fbba6d848a17153acc26f5f9134e7932b0ffa8
Olu <olu144@gmail.com>
2020-07-16 16:29:39 +0000

    remove commas

--- capstone/src/main/webapp/youtubeScript.js
+++ capstone/src/main/webapp/youtubeScript.js
@@ -30,7 +30,7 @@ async function callYoutube() {
   var response;
   /** Checks if input follows channel ID format, if not attempts to convert it to channel ID*/
   if (channelId[0] == "U" && channelId[1] == "C" && channelId.length == 24 && isLetter(channelId[channelId.length-1])) {
-    response = await fetch('/youtube_servlet?channelId=' + channelId,)
+    response = await fetch('/youtube_servlet?channelId=' + channelId)
     response = await response.json();
     if (response.hasOwnProperty('error')) {
       alert("Invalid Channel ID");
@@ -38,7 +38,7 @@ async function callYoutube() {
       return;
     }
   } else {
-    const usernameConverterResponse = await fetch('/youtube_username_servlet?channelId=' + channelId,)
+    const usernameConverterResponse = await fetch('/youtube_username_servlet?channelId=' + channelId)
     const usernameConverterResponseJson = await usernameConverterResponse.json();
     if (usernameConverterResponseJson.pageInfo.totalResults == 0) {
       alert("Username Not found, Please Input Channel ID");
@@ -46,7 +46,7 @@ async function callYoutube() {
       return;
     }
     const convertedUserName = usernameConverterResponseJson.items[0].id;
-    response = await fetch('/youtube_servlet?channelId=' + convertedUserName,)
+    response = await fetch('/youtube_servlet?channelId=' + convertedUserName)
     response = await response.json();
   }
   inputCommentsToPerspective(response);
