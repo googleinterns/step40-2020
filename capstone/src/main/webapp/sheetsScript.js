6a887577f98b83637c8e950569de629f3ff2d8e2
Daniel O <danieldortega77>
2020-07-17 21:04:51 +0000

    Add conditional formatting to Sheets output

--- capstone/src/main/webapp/sheetsScript.js
+++ capstone/src/main/webapp/sheetsScript.js
@@ -16,11 +16,11 @@
 const API_KEY = 'AIzaSyDMRYIXlcVWOVh-TTvrpVl11KTIw14Mg3c'; // TODO: Create Java servlet to return key
 
 // Array of API discovery doc URLs for APIs
-const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+const DISCOVERY_DOCS = ['https://sheets.googleapis.com/$discovery/rest?version=v4'];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
 
 /** Collects the user's input and submits it for analysis */
 async function gatherSheetsInput() {
@@ -41,7 +41,7 @@ async function gatherSheetsInput() {
   }
 
   // Get the selected attributes
-  const attributes = document.getElementById("available-attributes").getElementsByTagName('input');
+  const attributes = document.getElementById('available-attributes').getElementsByTagName('input');
   const requestedAttributes = [];
   for (const attribute of attributes) {
     if (attribute.checked) {
@@ -52,7 +52,7 @@ async function gatherSheetsInput() {
   // Get the selected analysis type
   document.getElementById('analysis-container').innerHTML = '';
   const radios = document.getElementsByName('analysisRadios');
-  let delimiter = "";
+  let delimiter = '';
   for (i = 0; i < radios.length; i++) {
     if (radios[i].checked) {
       delimiter = radios[i].value;
@@ -60,27 +60,27 @@ async function gatherSheetsInput() {
     }
   }
 
-  
+  // Show general output
+  const sheet = await getSpreadsheet(id);
+  const text = await getTextFromSheet(sheet);
+  handleInput(text, langElement.value, requestedAttributes, delimiter);
+
   // Create spreadsheet if requested
   const userDecisionElement = document.getElementById('sheets-output-yes-no');
   if (userDecisionElement != null && userDecisionElement.value === 'yes') {
     let title = 'Perspective Output';
     const titleElement = document.getElementById('sheets-title');
-    if (titleElement != null) {
+    if (titleElement != null && titleElement.value != '') {
       title = titleElement.value;
     }
 
     const newSheetId = await createSheet(title);
     const body = await createSheetOutput(id, langElement.value, requestedAttributes);
     await appendDataToSheet(newSheetId, body);
+    const numRows = body.length;
+    const numCols = body[0].length;
+    await addFormatting(newSheetId, numRows, numCols);
   }
-
-  
-
-  // Show general output
-  const sheet = await getSpreadsheet(id);
-  const text = await getTextFromSheet(sheet);
-  handleInput(text, langElement.value, requestedAttributes, delimiter);
 }
 
 /**
@@ -156,14 +156,45 @@ async function createSheetOutput(id, lang, requestedAttributes) {
 }
 
 /**
- * Append body to the Sheet with id spreadsheetId
+ * Append body to the Sheet with an id of id
  */
-async function appendDataToSheet(spreadsheetId, body) {
+async function appendDataToSheet(id, body) {
   await gapi.client.sheets.spreadsheets.values.append({
-    spreadsheetId: spreadsheetId,
+    spreadsheetId: id,
     range: 'Sheet1!A1:YY',
     valueInputOption: 'USER_ENTERED',
     resource: { values: body }
+  });
+}
+
+/**
+ * Color code number values within the row and column range
+ * in the Sheet with an id of id
+ */
+async function addFormatting(id, numRows, numCols) {
+  const myRange = {
+    sheetId: 0,
+    startRowIndex: 1,
+    endRowIndex: numRows + 1,
+    startColumnIndex: 1,
+    endColumnIndex: numCols + 1,
+  };
+  var requests = [{
+      addConditionalFormatRule: {
+        rule: {
+          ranges: [ myRange ],
+          gradientRule: {
+            minpoint: { color: { red: 0.345, green: 0.733, blue: 0.541, alpha: 1 }, type: 'NUMBER', value: '0' },
+            midpoint: { color: { red: 1, green: 0.839, blue: 0.4, alpha: 1 }, type: 'PERCENTILE', value: '50' },
+            maxpoint: { color: { red: 0.902, green: 0.486, blue: 0.451, alpha: 1}, type: 'NUMBER', value: '1' }
+          }
+        },
+        index: 0
+      }}];
+
+  await gapi.client.sheets.spreadsheets.batchUpdate({
+    spreadsheetId: id,
+    resource: { requests }
   });
 }
 
