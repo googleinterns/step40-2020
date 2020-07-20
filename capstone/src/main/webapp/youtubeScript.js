5fe7380a81336b751c011858a972dababac0450e
Olu <olu144@gmail.com>
2020-07-20 21:44:26 +0000

    optimize isLetter function

--- capstone/src/main/webapp/youtubeScript.js
+++ capstone/src/main/webapp/youtubeScript.js
@@ -177,8 +177,5 @@ function showAvailableAttributes() {
 
 /** Checks if a character is a letter */
 function isLetter(character) {
-  if ((character.charCodeAt() >= 65 && character.charCodeAt() <= 90) || (character.charCodeAt() >= 97 && character.charCodeAt() <= 122)) {
-    return true;
-  }
-  return false;
+  return (character.charCodeAt() >= 65 && character.charCodeAt() <= 90) || (character.charCodeAt() >= 97 && character.charCodeAt() <= 122); 
 }
