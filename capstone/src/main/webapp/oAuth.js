d570b0d4b3b78b0bf9eb4682153b3ae0ac1d2ef4
Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>
2020-07-29 14:22:57 -0700

    Google Docs Analysis (#23)
    * Implement Docs reading and analysis
    
    * Separate oAuth and Doc-specific JS files
    
    * Add comments to describe functions
    
    * Capitalize named button constants
    
    * Update to new version of script.js from #15
    
    * Add comment to readParagraphElement
    
    * Chenge name of handleClientLoad() to initOAuth()
    
    Co-authored-by: Daniel O <danieldortega77>

---
+++ capstone/src/main/webapp/oAuth.js
@@ -0,0 +1,80 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Client ID from the Developer Console 
+const CLIENT_ID = 'CLIENT_ID';
+
+const AUTHORIZE_BUTTON = document.getElementById('authorize_button');
+const SIGNOUT_BUTTON = document.getElementById('signout_button');
+const SUBMIT_BUTTON = document.getElementById('submit_button');
+
+/**
+  *  On load, called to load the auth2 library and API client library.
+  */
+function initOAuth() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+  *  Initializes the API client library and sets up sign-in state
+  *  listeners.
+  */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    AUTHORIZE_BUTTON.onclick = handleAuthClick;
+    SIGNOUT_BUTTON.onclick = handleSignoutClick;
+  }, function(error) {
+    console.log(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+  *  Called when the signed in status changes, to update the UI
+  *  appropriately. After a sign-in, the API is called.
+  */
+async function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    AUTHORIZE_BUTTON.style.display = 'none';
+    SIGNOUT_BUTTON.style.display = 'block';
+    SUBMIT_BUTTON.style.display = 'block';
+  } else {
+    AUTHORIZE_BUTTON.style.display = 'block';
+    SIGNOUT_BUTTON.style.display = 'none';
+    SUBMIT_BUTTON.style.display = 'none';
+  }
+}
+
+/**
+  *  Sign in the user upon button click.
+  */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+  *  Sign out the user upon button click.
+  */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
