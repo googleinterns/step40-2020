716a28c1674a603f5354aa0ad496604b67cdebb8
Daniel O <danieldortega77>
2020-07-31 20:53:32 +0000

    Comment jQuery functions

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -259,10 +259,12 @@ function drawBarChart(toxicityData) {
 
 /** Shows the avaiable attributes given a language selected on text analyzer page */
 function showAvailableAttributes() {
+  // Highlight <li> element when its input is checked
   $('.checkbox-menu').on('change', "input[type='checkbox']", function() {
     $(this).closest('li').toggleClass('active', this.checked);
   });
 
+  // Keep menu open when an option is selected or deselected
   $(document).on('click', '.allow-focus', function(e) {
     e.stopPropagation();
   });
@@ -297,6 +299,7 @@ function showAvailableAttributes() {
 }
 
 function loadAnalysisDropdown() {
+  // Highlight an <li> element in analysis radio selection when its input is checked
   $('.checkbox-menu').on('change', "input[type='radio']", function() {
     $("input[name='analysisRadios']").closest('li').toggleClass('active', false);
     $(this).closest('li').toggleClass('active', this.checked);
