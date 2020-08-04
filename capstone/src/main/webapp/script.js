dd67ba5cf4506cf10d53c2cc8efad569d0b45065
chrismohrii <65875732+chrismohrii@users.noreply.github.com>
2020-08-03 21:36:42 -0400

    Redo Datamuse Extremes Branch (#51)
    * Display basic text output for Perspective
    
    * Test
    
    * Post request to Perspective
    
    * Remove greeting in index.html
    
    * Remove hardcoding in servlet
    
    * Remove API key
    
    * Add user input box for servlet
    
    * Remove API key
    
    * Add newline at end of file
    
    * Remove API key
    
    * Add on Charts
    
    * Remove unnecessary space
    
    * Add interactive attribute selection
    
    * Remove key
    
    * Remove text output
    
    * Fix intern Daniel's nits
    
    * Fix Olu's nit
    
    * Implement Olu's style suggestion
    
    * Add detailed text analysis
    
    * Remove key
    
    * Add white background for text analyzer
    
    * Add newline
    
    * Commit Daniel's suggested change
    
    Co-authored-by: Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>
    
    * Improve text analyzer style
    
    * Improve text analyzer style & add tooltips
    
    * Fix overlap bug in detailed analysis
    
    * Fix code stlye of cover.css
    
    * Update border color of tooltip
    
    * Sort tooltip data in descending order
    
    * Fix intern Daniel's comments
    
    * Fix Olu's nit
    
    * Add more accurate breakup for sentence analysis
    
    * Remove honorifics detection (unrelated to this branch)
    
    * Clean up branch - error handling, spinners
    
    * Fix indents
    
    * Fix Lucas's comments
    
    * Fix Lucas's nits
    
    * Fix host Daniel's comments
    
    * Add customizable Datamuse suggestions
    
    * Add Datamuse graph results
    
    * Add about paragraph
    
    * Add extremes calculation
    
    * Fix an indent
    
    * Fix an indent
    
    * Fix intern Daniel's comments
    
    * Change to onclick for every word UX
    
    * Integrate into new UX
    
    * Highlight the changed word in extreme sentences
    
    * Fix minor code style issues
    
    * Fix Olu's nit
    
    * Fix intern Daniel's comment
    
    Co-authored-by: Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>
    
    * Optimize using parallel calls
    
    * Fix bug from merging
    
    * Place limit on number of API calls
    
    * Break up getExtremes method with helper methods
    
    * Improve a comment in getExtremes
    
    * Redesign Text Analyzer Flow using MD Bootstrap (#35)
    
    * Transition initial input to MDBootstrap
    
    * Fix regexp bug
    
    * Replace tooltips with sticky sidebar
    
    * Commit intern Daniel's nit
    
    Co-authored-by: Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>
    
    * Fix merging conflicts
    
    * Fix indentation in for loop
    
    Co-authored-by: Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>
    
    * Fix merge conflict
    
    Co-authored-by: Daniel O <danieldortega77>
    Co-authored-by: Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -36,6 +36,9 @@ const ATTRIBUTES_BY_LANGUAGE = {
   'pt': ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK', 'INSULT', 'PROFANITY', 'THREAT']
 };
 
+const MAX_TOTAL_DATAMUSE_RESULTS = 100;
+const MAX_DATAMUSE_RESULTS_PER_WORD = 10;
+
 /** Collects the user's input and submits it for analysis */
 async function gatherInput() {
   // Get the submitted text and language
@@ -58,7 +61,7 @@ async function gatherInput() {
   }
 
   // Get the selected analysis type
-  document.getElementById('general-analysis-container').innerHTML = '';
+  document.getElementById('colored-analysis-container').innerHTML = '';
   const radios = document.getElementsByName('analysisRadios');
   let tokenizer;
   for (i = 0; i < radios.length; i++) {
@@ -67,29 +70,27 @@ async function gatherInput() {
       break;
     }
   }
-
   handleInput(textElement.value, langElement.value, requestedAttributes, tokenizer);
 }
 
 /** Submits the input to Perspective and loads the appropriate output */
 async function handleInput(text, lang, requestedAttributes, tokenizer) {
-  // Remove any previous output
+	// Remove any previous output
+  document.getElementById('colored-analysis-container').innerHTML = '';
+  document.getElementById('segment-data').innerHTML = '';
+  document.getElementById('detailed-analysis-header').innerHTML = '';
   document.getElementById('perspective-datamuse-analysis').innerHTML = '';
   document.getElementById('perspective-datamuse-chart').innerHTML = '';
   document.getElementById('perspective-datamuse-extras').innerHTML = '';
-  document.getElementById('about-container').style.display = 'none';
-
-  // Draw the separating line for the output
-  const separator = document.getElementById('separator-container');
-  separator.innerHTML = '';
-  separator.appendChild(document.createElement('hr')); 
+  document.getElementById('perspective-datamuse-extremes').innerHTML = '';
+  document.getElementById('perspective-datamuse-header').style.display = 'none';
 	
   // Make Perspective call for the entire submission and load graph data
   const toxicityData = await callPerspective(text, lang, requestedAttributes);
   loadChartsApi(toxicityData);
 
   // Get detailed analysis if requested
-  if (tokenizer != 'undefined') {
+  if (tokenizer) {
     getAnalysis(text, lang, requestedAttributes, tokenizer); 
   }
 }
@@ -97,8 +98,10 @@ async function handleInput(text, lang, requestedAttributes, tokenizer) {
 /** Shows detailed analysis of text by word or sentence */
 async function getAnalysis(text, lang, requestedAttributes, tokenizer) {
   // Set up the detailed analysis section
-  const analysisContainer = document.getElementById('general-analysis-container');
-  analysisContainer.appendChild(createAnyElement('b', 'Detailed Anaylsis'));
+  const headerContainer = document.getElementById('detailed-analysis-header');
+  headerContainer.innerHTML = '';
+  headerContainer.appendChild(createAnyElement('h3', 'Detailed Anaylsis'));
+  const analysisContainer = document.getElementById('colored-analysis-container');
   const loadingEl = document.createElement('p');
   loadingEl.className = 'spinner-border';
   analysisContainer.appendChild(loadingEl);
@@ -117,7 +120,11 @@ async function getAnalysis(text, lang, requestedAttributes, tokenizer) {
     }
   });
 
+  // Print analysis and set up sidebar data element
   analysisContainer.removeChild(loadingEl);
+  const segmentDataEl = document.getElementById('segment-data');
+  segmentDataEl.innerHTML = '';
+  segmentDataEl.appendChild(createAnyElement('h5', 'Click on any segment for details'));	
   analysisContainer.appendChild(result);
 }
 
@@ -132,34 +139,154 @@ function addSubstring(substring, analysisContainer, result, loadingEl, response,
   const toxicityScore = response.attributeScores.TOXICITY.summaryScore.value;
   const attributes = sortAttributes(response);
 
-  // Set up replacements function call and color the substring appropriately	
+  // Break up and color the segment appropriately	
   const substringEl = document.createElement('span');
+  const wordElts = [];
   const words = getSubstrings(substring, TokenizerEnum['WORD']);
-  for (let i = 0; words && i < words.length; i++) {
-    const wordEl = createAnyElement('span', words[i]);
-    const bareWord = words[i].replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, ''); // Remove any punctuation & whitespace
-    wordEl.onclick = function() { setUpReplacements(substring, lang, bareWord); }
-    wordEl.classList.add('output-word'); 
-    substringEl.appendChild(wordEl);
+  if (words) {
+    for (let i = 0; i < words.length; i++) {
+      const wordEl = createAnyElement('span', words[i]);
+      const bareWord = words[i].replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, ''); // Remove any punctuation & whitespace
+      wordElts.push([wordEl, bareWord]);
+      substringEl.appendChild(wordEl);
+    }
+  } else {
+    substringEl.appendChild(createAnyElement('span', substring));
   }
   colorSubstring(substringEl, toxicityScore);
-     
-  // Attach a tooltip (info-box for the substring)
-  const tooltipEl = createTooltip(attributes);
-  substringEl.appendChild(tooltipEl);
+
+  // Set up analysis replacement function call after user clicks on segment
+  substringEl.onclick = function() { handleSegmentClick(attributes, substringEl, wordElts, substring, lang); }
   result.appendChild(substringEl);
 }
- 
+
+/** Appropriately sets the onclick attribute of a detailed analyis segment */ 
+function handleSegmentClick(attributes, substringEl, wordElts, substring, lang) {
+  showAttributes(attributes);
+  // Remove properties from previously clicked-on segments
+  const segments = document.getElementsByClassName('segment');
+  for (let i = 0; i < segments.length; i++) {
+    segments[i].classList.remove('purple-underline');
+    for (let j = 0; j < segments[i].childNodes.length; j++) {
+      segments[i].childNodes[j].className = '';
+      segments[i].childNodes[j].onclick = null;
+    }
+  }
+  // Allow replacements call for clicked-on segment
+  substringEl.classList.add('purple-underline');
+  for (let word of wordElts) {
+    word[0].onclick = function() { setUpReplacements(substring, lang, word[1]);};
+    word[0].classList.add('output-word');
+  }
+}
+
+/** Displays the attributes for a clicked-on segment */
+function showAttributes(attributes) {
+  const container = document.getElementById('segment-data');
+  container.innerHTML = '';
+  container.appendChild(createAnyElement('b', 'Top attributes'));
+  for (let i = 0; i < attributes.length && i < 3; i++) {
+    container.appendChild(createAnyElement('p', attributes[i][0] + ': ' + decimalToPercentage(attributes[i][1])));
+  }
+  container.appendChild(createAnyElement('b', 'Click on any word in selected segment for replacements'))
+}
+
 /** Sets up the replacements section */
 function setUpReplacements(text, lang, substringForReplacements) {
   // Clear previous results
   document.getElementById('perspective-datamuse-chart').innerHTML = '';
   document.getElementById('perspective-datamuse-extras').innerHTML = '';
 
-  document.getElementById('about-container').style.display = 'block';
+  document.getElementById('perspective-datamuse-header').style.display = 'block';
 
   getReplacements(text, lang, substringForReplacements);
   showDataMuseWordReplacementOptions(text, lang, substringForReplacements);
+}
+
+/** Gets the most extreme word replacements on a string */
+async function getExtremes(text, lang) {
+  // Remove any previous output & set up loading spinner
+  const container = document.getElementById('perspective-datamuse-extremes');
+  container.innerHTML = '';
+  const loadingEl = document.createElement('p');
+  loadingEl.className = 'spinner-border';
+  container.appendChild(loadingEl);
+
+  // Get the words & place a limit on the max number of requests
+  const words = text.split(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g); // Remove any punctuation or whitespace
+  let numResults = getNumResults(words.length);
+
+  // Get the new sentences and their scores
+  const replacements = await getAllReplacements(words, numResults);
+  const newSentences = [];
+  const styledSentences = [];
+  const promises = [];
+  for (let i = 0; i < replacements.length; i++) {
+    const newSentence = text.replace(replacements[i][0], replacements[i][1]);
+    const styledSentence = text.replace(replacements[i][0], '<b>' + replacements[i][1] + '</b>');
+    newSentences.push(newSentence);
+    styledSentences.push(styledSentence);
+    promises.push(callPerspective(newSentence, lang, ['TOXICITY']));
+  }
+
+  const mostToxic = { string: '', score: Number.MIN_VALUE };
+  const leastToxic = { string: '', score: Number.MAX_VALUE };
+  await storeExtremes(promises, mostToxic, leastToxic, styledSentences);
+  renderExtremes(container, loadingEl, leastToxic, mostToxic);
+}
+
+/** Finds the possible replacements of the words given. */
+async function getAllReplacements(words, numResults) {
+  const replacements = [];
+  const wordsCalled = [];
+  const datamuseCalls = [];
+  for (let word of words) {
+    for (let attribute of Object.values(DATAMUSE_ATTRIBUTES)) {
+      wordsCalled.push(word);
+      datamuseCalls.push(callDatamuse(word, attribute, numResults));
+    }
+  }
+  await Promise.all(datamuseCalls).then(datamuseResponses => {
+    for (let i = 0; i < datamuseResponses.length; i++) {
+      for (let j = 0; j < datamuseResponses[i].length; j++) {
+        replacements.push([wordsCalled[i], datamuseResponses[i][j].word]);
+      }
+    }
+  });
+  return replacements;
+}
+
+/** From an array of Perspective promises, stores the most extreme sentence variations in "mostToxic" & "leastToxic" */
+async function storeExtremes(promises, mostToxic, leastToxic, styledSentences) {
+  await Promise.all(promises).then(resolvedResponses => {
+    for (let i = 0; i < resolvedResponses.length; i++) {
+      const toxicityScore = resolvedResponses[i].attributeScores.TOXICITY.summaryScore.value;
+      // Update the current extreme values if necessary
+      if (toxicityScore > mostToxic.score) {
+        mostToxic.score = toxicityScore;
+        mostToxic.string = styledSentences[i];
+      } else if (toxicityScore < leastToxic.score) {
+        leastToxic.score = toxicityScore;
+        leastToxic.string = styledSentences[i];
+      }
+    }
+  });
+}
+
+/** Calculates the appropriate number of Datamuse results per word to get */
+function getNumResults(numWords) {
+  let numResults = MAX_DATAMUSE_RESULTS_PER_WORD;
+  if (numResults * numWords > MAX_TOTAL_DATAMUSE_RESULTS) {
+    numResults = Math.floor(MAX_TOTAL_DATAMUSE_RESULTS / numWords);
+  }
+  return numResults;
+}
+
+/** Renders the extreme variations of a sentence after they have been calculated */
+function renderExtremes(container, loadingEl, leastToxic, mostToxic) {
+  container.removeChild(loadingEl);
+  container.appendChild(createAnyElement('p', 'Least toxic variation: ' + leastToxic.string + ' -> ' + decimalToPercentage(leastToxic.score)));
+  container.appendChild(createAnyElement('p', 'Most toxic variation: ' + mostToxic.string + ' -> ' + decimalToPercentage(mostToxic.score)));
 }
 
 /** Gets the replacements & their score changes for a subtring based on the Datamuse API */
@@ -187,7 +314,7 @@ async function getReplacements(text, lang, substringForReplacements) {
   }
   
   // Get Datamuse API replacements of substring
-  const replacements = await callDatamuse(substringForReplacements, wordType);
+  const replacements = await callDatamuse(substringForReplacements, wordType, 5);
   if (replacements.length === 0) {
     analysisContainer.removeChild(loadingEl)
     analysisContainer.appendChild(createAnyElement('b', 'Datamuse API did not find any words or phrases matching your query'));
@@ -254,13 +381,35 @@ function showDataMuseWordReplacementOptions(text, lang, substringForReplacements
   container.innerHTML = '';
   container.appendChild(document.createElement('hr'));
 
+  const optionsRow = document.createElement('div');
+  optionsRow.className = 'row';
+
+  setUpWordTypeSelection(container, optionsRow, text, lang, substringForReplacements);
+  
+  // Set up extremes analysis button
+  const extremesSection = document.createElement('div')
+  extremesSection.className = 'col-6'; 
+  const extremesButton = document.createElement('button');
+  extremesButton.innerHTML = 'Get Extremes';
+  extremesButton.className = 'btn btn-primary';
+  extremesButton.onclick = function() { getExtremes(text, lang); };
+  extremesSection.appendChild(extremesButton)
+  optionsRow.appendChild(extremesSection);
+}
+
+function setUpWordTypeSelection(container, optionsRow, text, lang, substringForReplacements) {
+  const wordTypeSection = document.createElement('div');
+  wordTypeSection.className = 'col-6';
+	
   // Set up radios for word type selection
   const radiosEl = document.createElement('div');
   radiosEl.appendChild(createAnyElement('p', 'Different type of word replacement?'));
   for (let attribute of Object.keys(DATAMUSE_ATTRIBUTES)) {
     radiosEl.appendChild(createIndividualRadio(attribute + '-radio', 'radio', DATAMUSE_ATTRIBUTES[attribute], 'form-check', attribute, 'datamuse-radios'));
   }
-  container.appendChild(radiosEl);
+  wordTypeSection.appendChild(radiosEl);
+  optionsRow.appendChild(wordTypeSection);
+  container.appendChild(optionsRow);
   document.getElementsByName('datamuse-radios')[0].checked = true;
 
   // Set up submit button for word type selection
@@ -268,14 +417,14 @@ function showDataMuseWordReplacementOptions(text, lang, substringForReplacements
   submitButton.innerHTML = 'Submit';
   submitButton.className = 'btn btn-primary';
   submitButton.onclick = function() { getReplacements(text, lang, substringForReplacements); };
-  container.appendChild(submitButton);
+  wordTypeSection.appendChild(submitButton);
 }
 
 /** Gets the original toxicity score of the substring the user selected for replacements */
 async function getOriginalToxicity(text, lang, container, loadingEl) {
   const response = await callPerspective(text, lang, ['TOXICITY']);
   if (typeof(response.attributeScores) === 'undefined') {
-    container.removeChild(loadingEl)
+    container.removeChild(loadingEl);
     container.appendChild(createAnyElement('p', 'Perspective API was not able to get scores replacements'));
     return;
   }
@@ -316,9 +465,9 @@ function getReplacementsWordType() {
 }
  
 /** Gets a max of 5 word replacement replacement from Datamuse API */
-async function callDatamuse(text, wordType) {
+async function callDatamuse(text, wordType, numResults) {
   words = text.replace(' ', '+');
-  let response = await fetch('https://api.datamuse.com/words?' + wordType + '=' + words + '&max=5');
+  let response = await fetch('https://api.datamuse.com/words?' + wordType + '=' + words + '&max=' + numResults);
   return await response.json();
 }
 
@@ -347,39 +496,6 @@ function colorSubstring(substringEl, toxicityScore) {
   } else if (toxicityScore >= 0.2) {
     substringEl.className = 'yellow-background segment';
   }
-}
-
-/** Creates a tooltip for a substring in the detailed analysis */
-function createTooltip(attributes) {
-  const tooltipEl = document.createElement('div');
-  const headerEl = document.createElement('div');
-  const titleInfoEl = document.createElement('div');
-  const imageEl = document.createElement('img');
-  const bodyEl = document.createElement('div');
-  const titleEl = createAnyElement('h3', 'Perspective Feedback');
-  const subtitleEl = createAnyElement('p', 'click to get replacements for this word');
-
-  tooltipEl.className = 'tooltip';      
-  headerEl.className = 'header';
-  titleInfoEl.className = 'title-info';
-  titleEl.className = 'tooltip-title';
-  subtitleEl.className = 'tooltip-subtitle';
-  bodyEl.className = 'tooltip-body';
-
-  imageEl.setAttribute('src', 'assets/apple-touch-icon.png');
-  imageEl.setAttribute('alt', 'Perspective Logo');
-
-  titleInfoEl.appendChild(titleEl);
-  titleInfoEl.appendChild(subtitleEl);
-  headerEl.appendChild(imageEl);
-  headerEl.appendChild(titleInfoEl);
-  tooltipEl.appendChild(headerEl);
-  tooltipEl.appendChild(bodyEl);
-
-  for (const attribute of attributes) {
-    bodyEl.appendChild(createAnyElement('p', attribute[0] + ': ' + decimalToPercentage(attribute[1])));
-  }
-  return tooltipEl;
 }
 
 /** Converts decimals to percentages */
@@ -525,4 +641,23 @@ function showAvailableAttributes() {
     // Check the attribute's box; Ajax prevents doing this earlier
     list.children[0].children[0].checked = true;
   });
+}
+
+/** Shows or hides the advanced options for text analysis */
+function toggleAdvancedOptions() {
+  const toggleButton = document.getElementById('toggle-advanced-options-button');
+  if (!toggleButton) {
+    return;
+  }
+  const advancedOptionsEl = document.getElementById('advanced-options');
+  if (!advancedOptionsEl) {
+    return;
+  }
+  if (toggleButton.innerHTML === 'Advanced options') {
+    advancedOptionsEl.style.display = 'block';
+    toggleButton.innerHTML = 'Hide advanced options';
+  } else {
+    advancedOptionsEl.style.display = 'none';
+    toggleButton.innerHTML = 'Advanced options';
+  }
 }
