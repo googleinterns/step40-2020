4dd440bd04e995ad6b5cfa4e3d43a67c14af4c2f
Olu <olu144@gmail.com>
2020-08-07 00:13:59 +0000

    add package.json

--- capstone/src/main/java/com/google/sps/servlets/YoutubeServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/YoutubeServlet.java
@@ -44,8 +44,8 @@ public class YoutubeServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String channelId = request.getParameter("channelId");
     String videoId = request.getParameter("videoId");
-    String completeUrl = (ChannelId != null) ? 
-      (BASE_URL + "&allThreadsRelatedToChannelId=" + ChannelId + "&maxResults=" + NUM_RESULTS + "&key=" + KEY) : 
+    String completeUrl = (channelId != null) ? 
+      (BASE_URL + "&allThreadsRelatedToChannelId=" + channelId + "&maxResults=" + NUM_RESULTS + "&key=" + KEY) : 
         (BASE_URL + "&videoId=" + videoId + "&maxResults=" + NUM_RESULTS + "&key=" + KEY);
     String output = get(completeUrl);
     response.setContentType("application/json");
