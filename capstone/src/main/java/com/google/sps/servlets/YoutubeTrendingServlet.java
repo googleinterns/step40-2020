2cf01ed2a4826c79ec78f3e804c489fc215a3660
olu144 <54452415+olu144@users.noreply.github.com>
2020-08-07 18:55:11 -0400

    remove key

--- capstone/src/main/java/com/google/sps/servlets/YoutubeTrendingServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/YoutubeTrendingServlet.java
@@ -38,7 +38,7 @@ import com.google.sps.data.GetRequest;
 @WebServlet("/trending_servlet")
 public class YoutubeTrendingServlet extends HttpServlet {
   private static final String BASE_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular";
-  private static final String KEY = "AIzaSyCNknbH7wekG_bz1RcP1muXy9plNQhZaAY";
+  private static final String KEY = "API_KEY";
   private static final String NUM_RESULTS = "2";
   private static final MediaType JSON = MediaType.get("application/json; charset=utf-8"); 
   private ApiCaller apiCaller; 
